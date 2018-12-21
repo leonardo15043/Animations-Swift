98aa63580c7da2b9b16a2165828f82d230a3130e
leonardo15043 <lahernandez15043@gmail.com>
2018-12-21 17:04:34 -0500

    Animacion basica | ViewController:animateAction

--- AnimationsSwift/ViewController.swift
+++ AnimationsSwift/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var facePicture: UIImageView!
+    
+   
+    @IBAction func animateAction(_ sender: UIButton) {
+        
+        let animation:CABasicAnimation = CABasicAnimation(keyPath: "position")
+        //valor inicial de la animacion
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: facePicture.frame.midX, y: facePicture.frame.midY))
+        //especificamos el valor y para que la animacion tome esta posicion
+        animation.toValue = NSValue(cgPoint: CGPoint(x: facePicture.frame.midX, y: 640))
+        //la animación comience lentamente y luego se acelere a medida que avanza
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        animation.duration = 1.0
+        //agregamos la animacion a la imagen
+        facePicture.layer.add(animation, forKey: "position")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
